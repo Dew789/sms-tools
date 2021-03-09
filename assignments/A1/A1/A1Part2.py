722ce77b77ac4f200a0439aaef4d11bb542407dc
zhanghang <hangzhang2@iflytek.com>
2021-03-09 22:52:06 +0800

    A1 assignments

---
+++ assignments/A1/A1/A1Part2.py
@@ -0,0 +1,33 @@
+import sys
+import os
+sys.path.append('../../software/models/')
+sys.path.append('D:\\project2\\sms-tools\\software\\models')
+from utilFunctions import wavread
+
+"""
+A1-Part-2: Basic operations with audio
+
+Write a function that reads an audio file and returns the minimum and the maximum values of the audio 
+samples in that file. 
+
+The input to the function is the wav file name (including the path) and the output should be two floating 
+point values returned as a tuple.
+
+If you run your code using oboe-A4.wav as the input, the function should return the following output:  
+(-0.83486432, 0.56501967)
+"""
+def minMaxAudio(inputFile):
+    """
+    Input:
+        inputFile: file name of the wav file (including path)
+    Output:
+        A tuple of the minimum and the maximum value of the audio samples, like: (min_val, max_val)
+    """
+    ## Your code here
+    _, x = wavread(inputFile)
+
+    return min(x), max(x)
+
+if __name__ == '__main__':
+	result = minMaxAudio("D:\\project2\\sms-tools\\sounds\\oboe-A4.wav")
+	print(result)
